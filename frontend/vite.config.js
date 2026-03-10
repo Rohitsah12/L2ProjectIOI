7cf8b8ac70eb2ed3033d4a81011efca3ac466bf7
Deepali <deepalikumari62004@gmail.com>
2026-03-10 16:44:21 +0530

    changed vite config to make it work in localhost

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,6 +6,11 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
+   // Use root path for local dev; /CodeLytics/ for GitHub Pages production build
+   base: '/',
+   build:{
+      outDir: 'dist'
+   },
   plugins: [react(),tailwindcss()],
   resolve: {
     alias: {
